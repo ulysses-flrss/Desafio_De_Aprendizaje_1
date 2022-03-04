735d098dda84dff72af4b3ac04281c58d703d987
Guillermo-31 <guillermogr211@gmail.com>
2022-03-03 21:38:05 -0600

    Update Cotizacion

--- js/factura.js
+++ js/factura.js
@@ -1,24 +1,10 @@
 const iva = 0.13;
-const precio = 599;
-
-/*let nombre = document.getElementById.value;
-/*nombre.addEventListener("click",mostrarNombre);
-
-function mostrarNombre() {
-  alert(nombre);
-}*/
-
-//alert(nombre);
+const precio = 900;
 
 function crearFactura() {
   let nombre = document.getElementById("nombre").value;
   let email = document.getElementById("correo").value;
   let cantidad = document.getElementById("cantidad").value;
-  /*Swal.fire(
-    'Good job!',
-    'You clicked the button!',
-    'success'
-  )*/
   if (nombre == "") {
     Swal.fire(
       'Error',
@@ -41,14 +27,22 @@ function crearFactura() {
     let subtotal = precio*cantidad;
     let totalIva = subtotal*iva;
     let total = subtotal+totalIva;
+    let factura = document.getElementById("factura");
+    let ver = document.getElementById("verFact");
 
-    Swal.fire({
+    /*Swal.fire({
       title: 'FACTURA',
       html:
-      '<div class="textosFI"><p>Gracias por su compra: <b>'+nombre+' </b>.</p></div> <div class="factDescrip"> <table class="factura"><tr class="filas"><td class="colum1">Producto:</td><td class="colum2">Play Station 5</td></tr> <tr class="filas"><td class="colum1">Cantidad:</td><td class="colum2">'+cantidad+'</td></tr><tr><td class="colum1">Precio:</td><td class="colum2">$599.00</td></tr></table></div> <div class="cobros"> <div class="ley1"><p class="cLeyenda">SubTotal</p> <p class="cMoneda">$'+subtotal+'</p></div> <div class="ley2"><p class="cLeyenda">IVA (13%)</p> <p class="cMoneda">$'+totalIva+'</p></div> <div class="ley3"><p class="cLeyenda">Total a Pagar</p> <p class="cMoneda">$'+total+'</p></div></div> <div class="textosFI"><p>Se enviará una confirmación al correo: <b>'+email+' </b></p>',
+      '<div class="textosFI"><p>Gracias por su compra: <b>'+nombre+' </b>.</p></div> <div class="factDescrip"> <table class="fact"><tr class="filas"><td class="colum1">Producto:</td><td class="colum2">Play Station 5</td></tr> <tr class="filas"><td class="colum1">Cantidad:</td><td class="colum2">'+cantidad+'</td></tr><tr><td class="colum1">Precio:</td><td class="colum2">$599.00</td></tr></table></div> <div class="cobros"> <div id="ley1"><p class="cLeyenda">SubTotal</p> <p class="cMoneda">$'+subtotal+'</p></div> <div id="ley2"><p class="cLeyenda">IVA (13%)</p> <p class="cMoneda">$'+totalIva+'</p></div> <div id="ley3"><p class="cLeyenda">Total a Pagar</p> <p class="cMoneda">$'+total+'</p></div></div> <div class="textosFI"><p>Se enviará una confirmación al correo: <b>'+email+' </b></p>',
       showCloseButton: true
-    });
-    <div></div>
+    });*/
+
+    factura.innerHTML = "<div class='textosFI'><p>Gracias por su compra: <b>"+nombre+" </b>.</p></div> <div class='factDescrip'> <table class='fact'><tr class='filas'><td class='colum1'>Producto:</td><td class='colum2'>Play Station 5</td></tr> <tr class='filas'><td class='colum1'>Precio:</td><td class='colum2'>$"+precio+"</td></tr><tr><td class='colum1'>Cantidad:</td><td class='colum2'>"+cantidad+"</td></tr> <tr class='filas'><td class='colum1'>SubTotal</td> <td class='colum2'>$"+subtotal+"</td></tr> <tr><td class='colum1'>IVA (13%)</td> <td class='colum2'>$"+totalIva+"</td></tr> <tr><td class='colum1'>Total a Pagar</td> <td class='colum2'>$"+total+"</td></tr></div></table></div> <div class='textosFI'><p>Se enviará una confirmación al correo: <b>"+email+" </b></p>";
+
+    ver.style.opacity = '1';
+    factura.style.opacity = '1';
+    factura.style.transform = 'translateY(-10rem)';
+    factura.style.zIndex = '2';
   }
   //enviar.addEventListener("click",crearVariables);
 
